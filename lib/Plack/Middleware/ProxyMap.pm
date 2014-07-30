9ca56ce1570342b115274f8f47031b86b39f761c
Ingy döt Net <ingy@ingy.net>
2014-07-30 16:06:51 -0700

    CPAN Release 0.15

--- lib/Plack/Middleware/ProxyMap.pm
+++ lib/Plack/Middleware/ProxyMap.pm
@@ -1,6 +1,6 @@
 use strict; use warnings;
 package Plack::Middleware::ProxyMap;
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 use parent 'Plack::Middleware';
 use Plack::Util::Accessor qw(proxymap);
